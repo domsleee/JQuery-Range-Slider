f8159fa4de79686d457f59ab1f672f12afa9477a
Dom Slee <domslee97@gmail.com>
2016-08-31 16:25:46 +1000

    Split into src and dist folders, added demo skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // Minify the JS
+        uglify: {
+            my_target: {
+                files: {
+                    "dist/rangeslider.min.js": [
+                		"src/*.js"
+                    ]
+                }
+            }
+        },
+
+        // Minify the CSS
+        cssmin: {
+            my_target: {
+                files: [{
+                    expand:true,
+                    cwd:"src",
+                    src:["*.css"],
+                    dest:"dist/",
+                    ext:".min.css"
+                }]
+            }
+        }
+    });
+
+    // Import required tasks
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+    // Set default task to do everything
+    grunt.registerTask("default", ["uglify", "cssmin"]);
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Range Slider
+
+A JS & JQuery implementation of a range slider, inspired by Android's range sliders.
+
+<a href="google.com"><img width=235 src="https://puu.sh/qUHgg/3250a263da.png"></a>
