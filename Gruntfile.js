90349053c6d31136f34c34ca9ccb2c7e61389412
Dom Slee <domslee97@gmail.com>
2018-02-11 13:06:59 +1100

    Add gh-pages deploy to gruntfile as 'grunt deploy'

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,21 @@ module.exports = function(grunt) {
                     ext:".min.css"
                 }]
             }
+        },
+
+        shell: {
+            deploy: {
+                command: "git subtree push --prefix dist origin gh-pages"
+            }
         }
     });
 
     // Import required tasks
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-shell");
 
     // Set default task to do everything
     grunt.registerTask("default", ["uglify", "cssmin"]);
+    grunt.registerTask("deploy", ["uglify", "cssmin", "shell"]);
 };
